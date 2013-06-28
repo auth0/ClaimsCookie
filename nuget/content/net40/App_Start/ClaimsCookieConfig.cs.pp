af65d884accda9670e7bb25ff4e14b33596edd2e
siacomuzzi <sebastian.iacomuzzi@gmail.com>
2013-06-28 13:21:23 -0300

    included nuget source code

---
+++ nuget/content/net40/App_Start/ClaimsCookieConfig.cs.pp
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IdentityModel.Selectors;
+using Microsoft.IdentityModel.Web;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.ClaimsCookieConfig), "PreAppStart")]
+
+namespace $rootnamespace$.App_Start
+{
+    public static class ClaimsCookieConfig
+    {
+        public static void PreAppStart()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(ClaimsCookie.ClaimsCookieModule));
+        }
+    }
+}
---
+++ nuget/content/net45/App_Start/ClaimsCookieConfig.cs.pp
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof($rootnamespace$.App_Start.ClaimsCookieConfig), "PreAppStart")]
+
+namespace $rootnamespace$.App_Start
+{
+    public static class ClaimsCookieConfig
+    {
+        public static void PreAppStart()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(ClaimsCookie.ClaimsCookieModule));
+        }
+    }
+}
