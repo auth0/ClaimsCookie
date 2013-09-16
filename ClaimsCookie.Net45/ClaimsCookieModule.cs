d9e2a5173125da18d512e51e11d2550bbb63cd37
nicolas.sabena <nicolas@sabena.com.ar>
2013-09-16 17:36:07 -0300

    Changed method name + XML Comments.

--- ClaimsCookie.Net45/ClaimsCookieModule.cs
+++ ClaimsCookie.Net45/ClaimsCookieModule.cs
@@ -8,10 +8,21 @@
     using System.Linq;
     using System.Security.Claims;
 
+    /// <summary>
+    /// A custom implementation of a SessionAuthenticationModule that uses a
+    /// MachineKeySessionSecurityTokenHandler (to allow web farm scenarios),
+    /// and simplifies the creation of a session cookie.
+    /// </summary>
     public class ClaimsCookieModule : SessionAuthenticationModule
     {
+        /// <summary>
+        /// Occurs before the session cookie is created.
+        /// </summary>
         public EventHandler<BeforeCreateCookieEventArgs> BeforeCreateCookie;
 
+        /// <summary>
+        /// Gets the ClaimsCookieModule used by the web application.
+        /// </summary>
         public static ClaimsCookieModule Instance
         {
             get
@@ -20,6 +31,9 @@
             }
         }
 
+        /// <summary>
+        /// Overrides the default SecurityTokenHandler with a MachineKeySessionSecurityTokenHandler.
+        /// </summary>
         protected override void InitializePropertiesFromConfiguration()
         {
             if (this.FederationConfiguration != null)
@@ -28,7 +42,59 @@
             }
         }
 
-        public virtual void CreateSessionSecurityToken(IEnumerable<KeyValuePair<string, object>> user, string extraData = null, string domain = null, string path = null, bool requireSsl = false, bool httpOnly = true, string cookieName = null, TimeSpan? sessionCookieLifetime = null, bool persistent = false)
+        /// <summary>
+        /// Creates a ClaimsPrincipal from the specified user data, sets the current HTTP context and thread principal
+        /// and writes a session cookie from the resulting SessionSecurityToken using the specified parameters.
+        /// </summary>
+        /// <param name="user">The user data that will be used to create the claims for the ClaimsPrincipal. 
+        /// Array values are converted to multiple claims with the same key.</param>
+        /// <param name="extraData">An application defined context string.</param>
+        /// <param name="domain">The domain used for the session cookie.</param>
+        /// <param name="path">The virtual path used for the session cookie.</param>
+        /// <param name="requireSsl">Indicates if the session cookie should only be used with SSL.</param>
+        /// <param name="httpOnly">Indicates whether the session cookie should be hidden from client script.</param>
+        /// <param name="cookieName">Indicates the name of the session cookie.</param>
+        /// <param name="sessionCookieLifetime">The lifetime for the sesion cookie. A null value indicates no expiration.</param>
+        /// <param name="persistent">Indicates if the user agent should persist the session cookie. </param>
+        [Obsolete("This method is deprecated in favor of CreateSessionCookie() which should be used instead, with the same parameters.")]
+        public virtual void CreateSessionSecurityToken(
+            IEnumerable<KeyValuePair<string, object>> user,
+            string extraData = null,
+            string domain = null,
+            string path = null,
+            bool requireSsl = false,
+            bool httpOnly = true,
+            string cookieName = null,
+            TimeSpan? sessionCookieLifetime = null,
+            bool persistent = false)
+        {
+            this.CreateSessionCookie(user, extraData, domain, path, requireSsl, httpOnly, cookieName, sessionCookieLifetime, persistent);
+        }
+
+        /// <summary>
+        /// Creates a ClaimsPrincipal from the specified user data, sets the current HTTP context and thread principal
+        /// and writes a session cookie from the resulting SessionSecurityToken using the specified parameters.
+        /// </summary>
+        /// <param name="user">The user data that will be used to create the claims for the ClaimsPrincipal. 
+        /// Array values are converted to multiple claims with the same key.</param>
+        /// <param name="context">An application defined context string.</param>
+        /// <param name="domain">The domain used for the session cookie.</param>
+        /// <param name="path">The virtual path used for the session cookie.</param>
+        /// <param name="requireSsl">Indicates if the session cookie should only be used with SSL.</param>
+        /// <param name="httpOnly">Indicates whether the session cookie should be hidden from client script.</param>
+        /// <param name="cookieName">Indicates the name of the session cookie.</param>
+        /// <param name="sessionCookieLifetime">The lifetime for the sesion cookie. A null value indicates no expiration.</param>
+        /// <param name="persistent">Indicates if the user agent should persist the session cookie. </param>
+        public virtual void CreateSessionCookie(
+            IEnumerable<KeyValuePair<string, object>> user,
+            string context = null,
+            string domain = null,
+            string path = null,
+            bool requireSsl = false,
+            bool httpOnly = true,
+            string cookieName = null,
+            TimeSpan? sessionCookieLifetime = null,
+            bool persistent = false)
         {
             if (!string.IsNullOrEmpty(domain))
             {
@@ -49,11 +115,15 @@
             this.CookieHandler.HideFromClientScript = httpOnly;
 
             var claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name, user.First(a => a.Key == "name").Value.ToString()),
-                new Claim(ClaimTypes.NameIdentifier, user.First(a => a.Key == "user_id").Value.ToString()),
-                new Claim("http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider", user.First(a => a.Key == "connection").Value.ToString())
-            };
+                             {
+                                 new Claim(ClaimTypes.Name, user.First(a => a.Key == "name").Value.ToString()),
+                                 new Claim(
+                                     ClaimTypes.NameIdentifier,
+                                     user.First(a => a.Key == "user_id").Value.ToString()),
+                                 new Claim(
+                                     "http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider",
+                                     user.First(a => a.Key == "connection").Value.ToString())
+                             };
 
             foreach (var attribute in user)
             {
@@ -77,7 +147,14 @@
             var principal = new ClaimsPrincipal(new ClaimsIdentity[] { new ClaimsIdentity(claims, "Auth0") });
             this.OnBeforeCreateCookie(new BeforeCreateCookieEventArgs { Principal = principal });
 
-            var session = this.CreateSessionSecurityToken(principal, extraData, DateTime.UtcNow, sessionCookieLifetime.HasValue ? DateTime.UtcNow.Add(sessionCookieLifetime.Value) : DateTime.MaxValue.ToUniversalTime(), persistent);
+            var session = this.CreateSessionSecurityToken(
+                principal,
+                context,
+                DateTime.UtcNow,
+                sessionCookieLifetime.HasValue
+                    ? DateTime.UtcNow.Add(sessionCookieLifetime.Value)
+                    : DateTime.MaxValue.ToUniversalTime(),
+                persistent);
             this.AuthenticateSessionSecurityToken(session, true);
         }
 
